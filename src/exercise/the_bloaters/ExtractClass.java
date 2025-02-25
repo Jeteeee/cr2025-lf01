0e9d8ba58a30ab0857c807e345795b2882245537
Jeteeee <jonathantristanaditya@gmail.com>
2025-02-25 22:53:44 +0700

    Update ExtractClass.java

--- src/exercise/the_bloaters/ExtractClass.java
+++ src/exercise/the_bloaters/ExtractClass.java
@@ -1,18 +1,11 @@
 package exercise.the_bloaters;
 public class ExtractClass {
 // todo extract class TelephoneNumber from class Person
-	class Person {
-	  private String name;
-	  private String officeAreaCode;
-	  private String officeNumber;
-
-	  public String getName() {
-		return name;
-	  }
-	  public String getTelephoneNumber() {
-		return ("(" + officeAreaCode + ") " + officeNumber);
-	  }
-	  public String getOfficeAreaCode() {
+	class TelephoneNumber {
+		private String officeAreaCode;
+		private String officeNumber;
+	
+	 public String getOfficeAreaCode() {
 		return officeAreaCode;
 	  }
 	  public void setOfficeAreaCode(String arg) {
@@ -24,5 +17,44 @@ public class ExtractClass {
 	  public void setOfficeNumber(String arg) {
 		officeNumber = arg;
 	  }
+	   public String getTelephoneNumber() {
+		return ("(" + officeAreaCode + ") " + officeNumber);
+	  }
+		
+	}
+	
+	class Person {
+	  private String name;
+	  private TelephoneNumber TelephoneNumber;
+		public Person (){
+			this.TelephoneNumber = new TelephoneNumber();
+		}
+
+	  public String getName() {
+		return name;
+	  }
+	  public void setName(String name) {
+		  this.name = name
+	  }
+	  public String getTelephoneNumber() {
+            return telephoneNumber.getTelephoneNumber();
+        }
+
+        public String getOfficeAreaCode() {
+            return telephoneNumber.getOfficeAreaCode();
+        }
+
+        public void setOfficeAreaCode(String areaCode) {
+            telephoneNumber.setOfficeAreaCode(areaCode);
+        }
+
+        public String getOfficeNumber() {
+            return telephoneNumber.getOfficeNumber();
+        }
+
+        public void setOfficeNumber(String number) {
+            telephoneNumber.setOfficeNumber(number);
+        }
+    }
 	}
 }
